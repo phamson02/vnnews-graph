d82fdba17457a8cebe9edb69d4fa46cd71fd6ace
tungjav <tungchuyenthainguyen@gmail.com>
2023-02-02 21:44:01 +0700

    node form

--- src/views/DetailPanel.tsx
+++ src/views/DetailPanel.tsx
@@ -1,14 +1,23 @@
 import React, { FC, useState } from "react";
 import { BsInfoCircle } from "react-icons/bs";
-import FormSearch from "../components/node/FormSearch";
+import FormSearch from "../components/node/NodeSearchField";
 import { FiltersState } from "../types";
 
 import Panel from "./Panel";
 import SearchField from "./SearchField";
+import NodeSearchField from "../components/node/NodeSearchField";
 
 const DetailPanel: FC<{
   filters: FiltersState;
 }> = ({ filters }) => {
+  const [source, setSource] = useState<string | null>(null);
+  const [target, setTarget] = useState<string | null>(null);
+
+  const handleSearch = () => {
+    console.log(source, target);
+    
+  };
+
   return (
     <Panel
       title={
@@ -16,7 +25,21 @@ const DetailPanel: FC<{
           <BsInfoCircle className="text-muted" /> Details
         </>
       }>
-      <FormSearch filters={filters} />
+      <div className="search-wrapper">
+        <NodeSearchField
+          filters={filters}
+          selected={source}
+          setSelected={setSource}
+        />
+        <NodeSearchField
+          filters={filters}
+          selected={target}
+          setSelected={setTarget}
+        />
+        <button className="btn" onClick={handleSearch}>
+          search
+        </button>
+      </div>
     </Panel>
   );
 };
