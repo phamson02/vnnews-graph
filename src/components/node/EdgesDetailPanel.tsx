da727e28ba6bf273a2dd0de52d2481b767439b68
phamson02 <phamtienson02@gmail.com>
2023-02-02 22:34:34 +0700

    update

---
+++ src/components/node/EdgesDetailPanel.tsx
@@ -0,0 +1,18 @@
+import React, { FC, useEffect, useState } from "react";
+import { BsInfoCircle } from "react-icons/bs";
+import { useSigma } from "react-sigma-v2";
+
+const EdgesDetailPanel: FC<{
+  nodePath: string[];
+  edgePath: string[];
+}> = ({ nodePath, edgePath }) => {
+  const sigma = useSigma();
+  const graph = sigma.getGraph();
+
+  return (
+      <div className="search-wrapper">
+      </div>
+  );
+};
+
+export default EdgesDetailPanel;
--- src/views/DetailPanel.tsx
+++ src/views/DetailPanel.tsx
@@ -1,21 +1,82 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BsInfoCircle } from "react-icons/bs";
-import FormSearch from "../components/node/NodeSearchField";
 import { FiltersState } from "../types";
+import { dijkstra , edgePathFromNodePath } from 'graphology-shortest-path';
+import { useSigma } from "react-sigma-v2";
+
 
 import Panel from "./Panel";
-import SearchField from "./SearchField";
 import NodeSearchField from "../components/node/NodeSearchField";
+import EdgesDetailPanel from "../components/node/EdgesDetailPanel";
+
 
 const DetailPanel: FC<{
   filters: FiltersState;
 }> = ({ filters }) => {
+  const sigma = useSigma();
+  const graph = sigma.getGraph();
+
   const [source, setSource] = useState<string | null>(null);
   const [target, setTarget] = useState<string | null>(null);
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
+  let nodePath : string[] = [];
+  let edgePath : string[] = [];
+
+  useEffect(() => {
+    if (source && target) {
+      nodePath = dijkstra.bidirectional(graph, source, target);
+      edgePath = edgePathFromNodePath(graph, nodePath);
+
+      const sourceDisplayData = sigma.getNodeDisplayData(source);
+      const targetDisplayData = sigma.getNodeDisplayData(target);
+
+      if (sourceDisplayData && targetDisplayData)
+        sigma.getCamera().animate(
+          {
+            x: (sourceDisplayData.x + targetDisplayData.x) / 2,
+            y: (sourceDisplayData.y + targetDisplayData.y) / 2,
+            ratio: 1
+          },
+          {
+            duration: 600,
+          }
+        )
+
+      sigma.setSetting(
+        "nodeReducer", (node, data) => {
+          if (node === source || node === target) {
+            return { ...data, highlighted: true, color: "#00FF00" }
+          } else if (nodePath.includes(node)) {
+            return { ...data, highlighted: true, color: "#FF0000" }
+          } else {
+            return { ...data, highlighted: false, color: "#000000" }
+          }
+        }
+      );
+
+      sigma.setSetting(
+        "edgeReducer", (edge, data) => {
+          if (edgePath.includes(edge)) {
+            return { ...data, highlighted: true, color: "#FF0000" }
+          } else {
+            return { ...data, highlighted: false, color: "#000000" }
+          }
+        }
+      );
+
+      setShowDetails(true);
+    }
+  }, [source, target]);
 
   const handleSearch = () => {
-    console.log(source, target);
-    
+    if (source && target) {
+      const nodePath = dijkstra.bidirectional(graph, source, target);
+      console.log('path', nodePath)
+      const edgePath = edgePathFromNodePath(graph, nodePath);
+      console.log('edgePath', edgePath)
+
+    }
   };
 
   return (
@@ -40,6 +101,7 @@ const DetailPanel: FC<{
           search
         </button>
       </div>
+      {showDetails && <EdgesDetailPanel nodePath={nodePath} edgePath={edgePath}/>}
     </Panel>
   );
 };
