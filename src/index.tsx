cca313694511fa2a32add63afb439262d38c8f5e
phamson02 <phamtienson02@gmail.com>
2023-03-04 02:00:46 +0700

    revert update to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import "./styles.css";
 import Root from "./views/Root";
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
     <Root />,
-  </Router>,
   document.getElementById("root"),
 );
