d2e2694067bbb1ca581b52f931e2aa6bf8a2dfdf
phamson02 <phamtienson02@gmail.com>
2023-03-03 23:53:52 +0700

    deploy to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./styles.css";
 import Root from "./views/Root";
 
 ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
     <Root />,
+  </Router>,
   document.getElementById("root"),
 );
